d035db87a9f9c900b3664878beb53089ca2eb9c7
smith-steve <Steve.A.Dore@outlook.com>
2024-03-12 23:33:09 -0700

    final commit for the day.

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }]
+    }
+}
+
+/* This will take care of allowing us to ensure that babel is transpiling our code. */
